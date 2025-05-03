1fc3cc4bc6d893736403e5c1700f7adff4a418e8
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2025-05-03 20:20:19 +0530

    AppReducerFix

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,16 +1,20 @@
-export default (state, action) => {
-  switch(action.type) {
-    case 'DELETE_TRANSACTION':
+const AppReducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
       return {
         ...state,
-        transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
-      }
-    case 'ADD_TRANSACTION':
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    case "ADD_TRANSACTION":
       return {
         ...state,
-        transactions: [action.payload, ...state.transactions]
-      }
+        transactions: [action.payload, ...state.transactions],
+      };
     default:
       return state;
   }
-}
+};
+
+export default AppReducer;
